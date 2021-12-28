fa158b98185a32ba3b667678d80ced27d065fe11
Jordan Johnson <jordan.johnson.dev@gmail.com>
2021-12-27 22:45:46 -0500

    Updated for linter

--- src/domain/abstract/field_types/text.ts
+++ src/domain/abstract/field_types/text.ts
@@ -1,30 +1,38 @@
-import { FieldType, IField } from '../ifield';
+import { FieldType, IField } from '../../../models/ifield';
 
-export class Text implements IField {
-    name: string = 'undefined';
-    type: FieldType = 'text';
-    value: string = '';
+class Text implements IField {
+  name: string = 'undefined';
 
-    minLength?: number;
-    maxLength?: number;
-    
-    constructor(name: string, options?: {
-        defaultValue: string,
-        minLength?: number, 
-        maxLength?: number
-    }) {
-        this.name = name;
-        this.value = options?.defaultValue ?? '';
-        this.minLength = options?.minLength;
-        this.maxLength = options?.maxLength;
+  type: FieldType = 'text';
+
+  value: string = '';
+
+  minLength?: number;
+
+  maxLength?: number;
+
+  constructor(
+    name: string,
+    options?: {
+      defaultValue: string;
+      minLength?: number;
+      maxLength?: number;
     }
+  ) {
+    this.name = name;
+    this.value = options?.defaultValue ?? '';
+    this.minLength = options?.minLength;
+    this.maxLength = options?.maxLength;
+  }
 
-    validate(): void {
-        // check if value is longer than max length
-        if(this.value && this.maxLength) {
-            if(this.value?.length > this.maxLength) {
-                throw new Error('Value exceeds maximum length');
-            }
-        }
+  validate(): void {
+    // check if value is longer than max length
+    if (this.value && this.maxLength) {
+      if (this.value?.length > this.maxLength) {
+        throw new Error('Value exceeds maximum length');
+      }
     }
+  }
 }
+
+export default Text;
