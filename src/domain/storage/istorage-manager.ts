afb5dacc19735cd4816c9eada34d23b160503aa7
Jordan Johnson <jordan.johnson.dev@gmail.com>
2021-12-27 22:47:11 -0500

    Removed json storage
    May use nedb instead

--- src/domain/storage/istorage-manager.ts
+++
@@ -1,7 +0,0 @@
-import { IObject } from "../abstract/iobject";
-
-export interface IStorageManager {
-    exists(obj: IObject): boolean;
-    post(obj: IObject): Promise<void>;
-    delete(obj: IObject): Promise<void>;
-}
